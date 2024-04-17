86edc8fdfad507751bc0de169dcdfb32735be1e9
DeFiNiek <definiek@gmail.com>
2024-04-17 19:26:37 +0200

    first commit

--- components/data.js
+++ components/data.js
@@ -7,8 +7,8 @@ import {
   SignalIcon,
 } from "@heroicons/react/24/solid";
 
-import benefitOneImg from "../public/img/benefit-one.png";
-import benefitTwoImg from "../public/img/benefit-two.png";
+import benefitOneImg from "/public/img/benefit-one.png";
+import benefitTwoImg from "/public/img/benefit-two.png";
 
 const benefitOne = {
   title: "What I can do for you.",
